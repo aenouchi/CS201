65e859efa8d1cbd5d1ea27479a46bfab3d3d3452
aenouchi <59933036+aenouchi@users.noreply.github.com>
2020-01-19 11:30:28 -0900

    Add files via upload

--- Homework0/HelloWorld2/Source.cpp
+++ Homework0/HelloWorld2/Source.cpp
@@ -3,10 +3,23 @@
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World" << std::endl;
+	using namespace std;
 
 	int x;
-	std::cin >> x;
+	for (x=0;x<5;x++)
+	cout << "Hello World" << endl;
+
+	cout << "Pen in hand, I find my strength." << endl;
+	cout << "The courage endowed upon me by my one and only love." << endl;
+	cout << "Together, let us dismantle this crumbling world" << endl;
+	cout << "And write a novel of our own fantasies" << endl;
+	cout << "With a flick of her pen, the lost finds her way." << endl;
+	cout << "In a world of infinite choices, behold this special day." << endl;
+	cout << "After all," << endl;
+	cout << "Not all good times must come to an end "<< endl;
+
+	cout << " Super Smash Bros.: Nintendo characters and other Japanese videogame characters fight. ";
+	
 
 	return 0;
 }
