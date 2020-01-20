01a1ab243960d5098cd38186f91206d8122c8b41
Aaron <aenouchi@alaska.edu>
2020-01-19 19:42:02 -0900

    The code for Homework 0

--- Homework0/HelloWorld2/Poem.cpp
+++ Homework0/HelloWorld2/Poem.cpp
@@ -6,8 +6,8 @@ int main(int argc, char** argv)
 	using namespace std;
 
 	int x;
-	for (x=0;x<5;x++)
-	cout << "Hello World" << endl;
+	for (x = 0; x < 5; x++)
+		cout << "Hello World" << endl;
 
 	cout << "Pen in hand, I find my strength." << endl;
 	cout << "The courage endowed upon me by my one and only love." << endl;
@@ -16,10 +16,10 @@ int main(int argc, char** argv)
 	cout << "With a flick of her pen, the lost finds her way." << endl;
 	cout << "In a world of infinite choices, behold this special day." << endl;
 	cout << "After all," << endl;
-	cout << "Not all good times must come to an end "<< endl;
+	cout << "Not all good times must come to an end " << endl;
 
 	cout << " Super Smash Bros.: Nintendo characters and other Japanese videogame characters fight. ";
-	
+
 
 	return 0;
 }
