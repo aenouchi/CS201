3f80424cf75de9bc18187fe7c893532819928e00
Aaron <aenouchi@alaska.edu>
2020-02-01 08:12:10 -0900

    Input message for negative numbers

--- HW1/mileskm.cpp
+++ HW1/mileskm.cpp
@@ -17,9 +17,15 @@ using std::cin;
 
 int main()
 	{
-		double mile ;
-		cout << "Please input the miles to be converted (positive numbers only)" << endl;
-		cin >> mile;
-		cout <<(mile * 1.609) << " kilometers" << endl;
+		double mile; 
+			cout << "Input the miles to be converted to km (positive numbers only)" << endl;
+			cin >> mile;
+			if (mile >= 0) {
+				cout << (mile * 1.609) << " kilometers" << endl;
+			}
+			else if (mile < 0)
+				{
+				cout << "I said POSITIVE numbers" << endl;
+			}
 
 	}
