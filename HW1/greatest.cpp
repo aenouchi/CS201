e315370b644b3a1a388b96d32605fcba7e59f9ed
Aaron <aenouchi@alaska.edu>
2020-02-01 12:31:28 -0900

    Errors in semicolons removed

--- HW1/greatest.cpp
+++ HW1/greatest.cpp
@@ -23,14 +23,14 @@ int main()
 	int n;
 	int m = 0;
 	vector <int> l;
-	int greatest = 0
+	int greatest = 0;
 	
 	cout << "Enter sequence of positive integers, ending with zero. I will print the greatest positive integer." << endl;
 	cin >> n;
 	while (n > m) {
 		cout << "Enter positive integer (0 to end)" << n << endl;
 		cin >> n;
-		l.push_back{ n };
+		l.push_back(n);
 		for (int i = 0; i < l.size(); i++) {
 			if (n > greatest)
 			{
