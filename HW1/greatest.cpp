ca0c49d67d23890971944621d562786d10f07e8f
Aaron <aenouchi@alaska.edu>
2020-02-01 12:23:27 -0900

    Establishing greatest number

--- HW1/greatest.cpp
+++ HW1/greatest.cpp
@@ -10,24 +10,36 @@
 #include <cmath>
 #include <iomanip>
 #include <limits>
+#include <vector>
+using std::vector;
 using namespace std;
 using std::cout;
 using std::endl;
 using std::cin;
 
 
+
 int main()
 {
 	int n;
 	int m = 0;
+	vector <int> l;
+	int greatest = 0
+	
 	cout << "Enter sequence of positive integers, ending with zero. I will print the greatest positive integer." << endl;
 	cin >> n;
 	while (n > m) {
 		cout << "Enter positive integer (0 to end)" << n << endl;
 		cin >> n;
-
+		l.push_back;{ n; };
+		for (int i = 0; i < l.size(); i++) {
+			if (n > greatest);
+			{
+				n = greatest;
+			}
+			}
 	}
-	while (n < m)
+	if (n < m)
 	{
 		cout << "no positive inputs detected" << endl;
 		break;
