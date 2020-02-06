020da3b80e0218fc7a7fff90ecd95161d8910842
Aaron <aenouchi@alaska.edu>
2020-02-05 19:41:52 -0900

    For loop that takes 10 names.

--- Homework 2/names.cpp
+++ Homework 2/names.cpp
@@ -1,4 +1,18 @@
-int main()
-{
+/**
+*Homework 1
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*February 5,2019
+*/
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main(int argc, char **argv)
+{
+	std::vector<std::string> names;
+	for (int i = 0; i< 10; i++ )
 }
