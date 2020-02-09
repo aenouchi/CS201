4e0abd9ce218a1c7637ff8379282a2c42074c714
Aaron <aenouchi@alaska.edu>
2020-02-09 12:36:46 -0900

    First message established "You have ... pennies/penny"

--- Homework 2/money4.cpp
+++ Homework 2/money4.cpp
@@ -26,16 +26,32 @@ int main()
 
 	cout << "How many pennies do you have?" << endl;
 	cin >> pennies;
+	
 	cout << "How many nickels do you have?" << endl;
 	cin >> nickels;
+	
 	cout << "How many dimes do you have?" << endl;
 	cin >> dimes;
+	
 	cout << "How many quarters do you have?" << endl;
 	cin >> quarters;
+	
 	cout << "How many half dollars do you have?" << endl;
 	cin >> halfdollars;
+	
 	cout << "How many dollars do you have?" << endl;
 	cin >> dollars;
+
+	if (pennies != 1)
+		cout << " You have " << pennies << " pennies" << endl;
+	else
+		cout << "You have 1 penny" << endl;
+
+
+
+
+
+
 
 
 
