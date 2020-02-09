9cbc6e85653125cf121fd504e3943385b990d539
Aaron <aenouchi@alaska.edu>
2020-02-09 12:33:08 -0900

    All user inputs established

--- Homework 2/money4.cpp
+++ Homework 2/money4.cpp
@@ -26,6 +26,17 @@ int main()
 
 	cout << "How many pennies do you have?" << endl;
 	cin >> pennies;
+	cout << "How many nickels do you have?" << endl;
+	cin >> nickels;
+	cout << "How many dimes do you have?" << endl;
+	cin >> dimes;
+	cout << "How many quarters do you have?" << endl;
+	cin >> quarters;
+	cout << "How many half dollars do you have?" << endl;
+	cin >> halfdollars;
+	cout << "How many dollars do you have?" << endl;
+	cin >> dollars;
+
 
 
 }
