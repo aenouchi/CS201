6a3eaa03b07a7bf52512c43ab582811bd0518419
Aaron <aenouchi@alaska.edu>
2020-02-16 10:16:49 -0900

    boxerhpp is contains message  function

---
+++ Homework 3/Boxer.cpp
@@ -0,0 +1,18 @@
+/**
+*Boxer.cpp
+*Homework 3
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*February 15,2019
+*/
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+#include "Boxer.hpp"
+
+int main()
+
+{}
---
+++ Homework 3/Boxer.hpp
@@ -0,0 +1,19 @@
+/**
+*Boxer.hpp
+*Homework 3
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*February 15,2019
+*/
+
+#ifndef boxer_hpp
+#define boxer_hpp
+
+#include <string>
+#include <iostream>
+
+void box(std::string word, int x); //Establish message for box
+
+#endif
+
