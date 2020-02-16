949354f9ffd9f4a284eb7dec39a289e954c9f951
Aaron <aenouchi@alaska.edu>
2020-02-16 10:46:38 -0900

    Integers establish in boxprint.cpp

---
+++ Homework 3/Boxprint.cpp
@@ -0,0 +1,20 @@
+/**
+*Boxer.cpp
+*Homework 3
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*February 15,2019
+*/
+#include "Boxer.hpp"
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+	int x;
+	std::string word;
+
+
+}
