957dc9f6349763ededd51cd0b8afa8d55164d82d
Aaron Nouchi <aenouchi@alaska.edu>
2020-02-23 12:12:11 -0900

    Testing values established

--- Homework 4/tokenizertest.cpp
+++ Homework 4/tokenizertest.cpp
@@ -7,7 +7,15 @@
 *February 22,2020
 */
 #include <iostream>
+#include "tokenizer.hpp"
+#include <vector>
+
+using std::vector;
+using std::cout;
+
 
 int main()
 {
+	vector <std::string> mv = { "HelloWorld","Sonic1991", $$$$,"012345689" };
+	
 }
