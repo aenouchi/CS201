69cdaf167475b45e84be50feb243c44ce4564782
Aaron Nouchi <aenouchi@alaska.edu>
2020-02-24 15:27:00 -0900

    Input for name and color

--- Homework 4/crud.cpp
+++ Homework 4/crud.cpp
@@ -16,7 +16,7 @@ using std::string;
 
 void menu()
 {
-	std::string ip;
+	string ip;
 	cout << "Welcome to Create Retrieve Update and Delete (CRUD), what would you like to do?" << endl;
 	cout << " create \nread \nupdate \delete \nend";
 	cin >> ip;
@@ -25,5 +25,13 @@ void menu()
 
 void see()
 {
+	string i1;
+	string i2;
 
+		cout << "Input put a name" << endl;
+		cin >> i1;
+		cout << "\nInput their favourite color: ";
+		cin >> i2;
+
+		create(i1, i2);
 }
