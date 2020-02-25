db8d3b76d8e9e56edbc6bb0dbe27a0c615c61321
Aaron Nouchi <aenouchi@alaska.edu>
2020-02-24 19:35:13 -0900

    u and y replaced with c and b

--- Homework 4/bulls-and-cows.cpp
+++ Homework 4/bulls-and-cows.cpp
@@ -11,18 +11,20 @@
 
 void check(int x, int g1, int g2, int g3, int g4)
 {
-	int u = 0;
-	int y;
+	int c = 0;
+	int b;
 
 	if (x % 10 == g4)
-		u++;
+		c++;
 	if ((x % 100) - (x % 10) == g3 * 10)
+		c++;
+	if ((c % 1000) - (x % 100) == g2 * 100)
+		c++;
+	if (x- ((x % 1000) - ((x % 100))) == g2 * 1000)
 		u++;
-	if ((u % 1000) - (x % 100) == g2 * 100)
-		u++;
-	if (x- ((u % 1000) - ((x % 100))) == g2 * 1000)
-		u++;
+	y = 4 - c;
 
+	if (c !=0 && b != 0)
 }
 
 int main()
