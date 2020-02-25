32c39c93362d167286307b355ff1aee31c148dd4
Aaron Nouchi <aenouchi@alaska.edu>
2020-02-24 19:31:47 -0900

    if divisibles established

--- Homework 4/bulls-and-cows.cpp
+++ Homework 4/bulls-and-cows.cpp
@@ -11,8 +11,17 @@
 
 void check(int x, int g1, int g2, int g3, int g4)
 {
-	int c = 0;
-	int b;
+	int u = 0;
+	int y;
+
+	if (x % 10 == g4)
+		u++;
+	if ((x % 100) - (x % 10) == g3 * 10)
+		u++;
+	if ((u % 1000) - (x % 100) == g2 * 100)
+		u++;
+	if (x- ((u % 1000) - ((x % 100))) == g2 * 1000)
+		u++;
 
 }
 
