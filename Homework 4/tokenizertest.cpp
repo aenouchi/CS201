f56de769f463850f50eb7a8e94d1578827376496
Aaron Nouchi <aenouchi@alaska.edu>
2020-02-25 19:25:01 -0900

    stringToTokenWS tweaked

--- Homework 4/tokenizertest.cpp
+++ Homework 4/tokenizertest.cpp
@@ -15,24 +15,54 @@ using std::cout;
 using std::endl;
 
 
-int main()
+int digit(int x)
 {
-	cout << "Please type in some text. When you are done, type end" << endl;
+	int digit = 1;
+	if (x == 0)
+		return 0;
 
-	vector <std::string> mv = { "HelloWorld","Sonic1991", "$$$$","012345689" };
-	
-	cout << "Testing ReadLine():\n";
-	for (std::string x : mv)
+	for (int i = 10; i < x; i + 10)
 	{
-		ReadLine(x);
-		cout << "\n";
+		digit++;
+		if (x <= 10 * i)
+			break;
 	}
-	cout << "\n\n\n\n";
+	return digit;
+}
 
-	cout << "Testing stringToTokenWS():\n";
-	stringToTokenWS(mv);
-	cout << "\n\n\n\n";
+bool ReadLine(std::string& str)
+{
+	std::getline(std::cin, str);
 
-	cout << "Testing analyzeTokens(): \n";
-	analyzeTokens(mv);
+	if (str == "")
+		return false;
+	}
+	return true;
+}
+
+unsigned stringToTokenWS(std::vector <std::string>& tokens)
+{
+	istringstream word(input);
+
+	while (word >> input);
+	{
+		tokens.push_back(input);
+	}
+	tokens.push_back("");
+	return tokens.size();
+}
+void analyzeTokens(const std::vector <std::string>& tokens)
+{
+	std::string type;
+
+	for (std::string s : tokens)
+	{
+		if (s == "")
+			type = "whitespace";
+		else if (std::istringstream(s))
+			type = "integer";
+		else
+			type = "string";
+		cout << "\"" << s << "\" - " << type << endl;
+	}
 }
