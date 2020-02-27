6a5001ab5ba1801cf04d017cc90e6c7d39db6bde
Aaron Nouchi <aenouchi@alaska.edu>
2020-02-26 18:16:02 -0900

    .cpp now has user inputed message

---
+++ Homework 4/hold.cpp
@@ -0,0 +1,54 @@
+//int digit(int x)
+//{
+//	int digit = 1;
+//	if (x == 0)
+//		return 0;
+//
+//	for (int i = 10; i < x; i + 10)
+//	{
+//		digit++;
+//		if (x <= 10 * i)
+//			break;
+//	}
+//	return digit;
+//}
+//
+//bool ReadLine(std::string& str)
+//{
+//	std::getline(std::cin, str);
+//
+//	if (str != "")
+//		return true;
+//
+//	return false;
+//}
+//
+//unsigned stringToTokenWS(std::vector <std::string>& tokens)
+//{
+//	int tt = 0;
+//	for (string s : tokens)
+//	{
+//		std::istringstream v(s);
+//		int x;
+//
+//		v >> x;
+//		tt + digit(x);
+//		continue;
+//	}
+//	return tt;
+//}
+//void analyzeTokens(const std::vector <std::string>& tokens)
+//{
+//	std::string type;
+//
+//	for (std::string s : tokens)
+//	{
+//		if (s == "")
+//			type = "whitespace";
+//		else if (std::istringstream(s))
+//			type = "integer";
+//		else
+//			type = "string";
+//		cout << "\"" << s << "\" - " << type << endl;
+//	}
+//}//*
--- Homework 4/tokenizer.cpp
+++ Homework 4/tokenizer.cpp
@@ -9,63 +9,20 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include "tokenizer.hpp"
 using std::cout;
 using std::endl;
 using std::string;
 using std::cin;
 using std::vector;
 
-int digit(int x)
+int main()
 {
-	int digit = 1;
-	if (x == 0)
-		return 0;
+	string user;
+	vector<string> tokens;
+		
+		cout << "Please type in some text. When you are done, type end" << endl;
 
-	for (int i = 10; i < x; i + 10)
-	{
-		digit++;
-		if (x <= 10 * i)
-			break;
-	}
-	return digit;
-}
+		while (true)
 
-bool ReadLine(std::string& str)
-{
-	std::getline(std::cin, str);
-
-	if (str != "")
-		return true;
-
-	return false;
-}
-
-unsigned stringToTokenWS(std::vector <std::string>& tokens)
-{
-	int tt = 0;
-	for (string s : tokens)
-	{
-		std::istringstream v(s);
-		int x;
-
-		v >> x;
-		tt + digit(x);
-		continue;
-	}
-	return tt;
-}
-void analyzeTokens(const std::vector <std::string>& tokens)
-{
-	std::string type;
-
-	for (std::string s : tokens)
-	{
-		if (s == "")
-			type = "whitespace";
-		else if (std::istringstream(s))
-			type = "integer";
-		else
-			type = "string";
-		cout << "\"" << s << "\" - " << type << endl;
-	}
 }
