d841bc96e98ea10c325a0dd623527686d3fc9554
Aaron Nouchi <aenouchi@alaska.edu>
2020-03-26 17:39:11 -0800

    Basics

---
+++ Lab 20/Source.cpp
@@ -0,0 +1,20 @@
+
+/**
+*Source.cpp
+*Lab 20
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+* March 25,2020
+*/
+
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+
