ab620a0c3f9f62ce40b2e6a361b629b2ad1db237
Aaron Nouchi <aenouchi@alaska.edu>
2020-03-30 18:24:53 -0800

    Installed fltk-trunc

--- Homework 5/fltk-trunc.cpp
+++ Homework 5/fltk-trunc.cpp
@@ -11,6 +11,9 @@
 #include <string>
 #include <vector>
 #include <sstream> 
+#include<FL/Fl.h>
+#include<FL/Fl_Box.h>
+#include<FL/Fl_Window.h>
 using std::istringstream;
 using std::string;
 using std::vector;
@@ -19,5 +22,8 @@ using std::endl;
 
 int main()
 {
-
+    Fl_Window window(200, 200, "Window title");
+    Fl_Box box(0, 0, 200, 200, "Hey, I mean, Hello, World!");
+    window.show();
+    return Fl::run();
 }
