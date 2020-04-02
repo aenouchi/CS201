65a9612e44e127ff3068fd20646c63f00554cd74
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-01 20:29:27 -0800

    Basic

--- Homework 5/fltk-trunc.cpp
+++ Homework 5/fltk-trunc.cpp
@@ -26,4 +26,4 @@ int main()
     Fl_Box box(0, 0, 200, 200, "Hey, I mean, Hello, World!");
     window.show();
     return Fl::run();
-}
+} 
---
+++ Lab 21/Source.cpp
@@ -0,0 +1,10 @@
+/**
+*Source.cpp
+*Lab21
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*March 30,2020
+*/
+
+#include <iostream>
