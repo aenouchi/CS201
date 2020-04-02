a406f92740f66df5f3003bf464c3f410751d0bba
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-01 20:50:28 -0800

    Files added

---
+++ Lab 21/main.cpp
@@ -0,0 +1,18 @@
+/**
+*Source.cpp
+*Lab21
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*March 30,2020
+*/
+
+#include "truncstruct.hpp"
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+
+}
