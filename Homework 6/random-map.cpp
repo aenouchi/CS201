5bbbeb7157555018061496023e0cf2827ba56f01
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-04 16:04:29 -0800

    headers added

---
+++ Homework 6/random-map.cpp
@@ -0,0 +1,27 @@
+/**
+*random-map.cpp
+*Homework 6
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 4,2020
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <random>
+#include <utility>
+#include <map>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::map;
+
+int main()
+{
+
+}
