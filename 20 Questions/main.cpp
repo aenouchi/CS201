3f5a7f3c3e56400ffbea991eabc6a244b95a129f
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-04 19:26:24 -0800

    Spaces added.

--- 20 Questions/main.cpp
+++ 20 Questions/main.cpp
@@ -13,80 +13,80 @@ using std::cin;
 
 int main()
 {
-	cout << " Think of a common animal you would find on a farm." << endl;
-	cout << "I will try to guess the animal within ten questions" << endl;
+	cout << " Think of a common animal you would find on a farm. \n"  << endl;
+	cout << "I will try to guess the animal within ten questions. \n" << endl;
 
-	cout << "Does your animal typically walk on four legs? (yes/no)" << endl;
+	cout << "Does your animal typically walk on four legs? (yes/no)\n" << endl;
 	
-	cout << "Does your animal have hooves? (yes/no)" << endl;
+	cout << "Does your animal have hooves? (yes/no)\n" << endl;
 
-	cout << "Is it a hybrid animal? (yes/no)" << endl;
+	cout << "Is it a hybrid animal? (yes/no)\n" << endl;
 
-	cout << "Is it a Mule?" << endl;
+	cout << "Is it a Mule?\n" << endl;
 
-	cout << "Is your animal know to be ridden on for many miles? (yes/no)" << endl;
+	cout << "Is your animal know to be ridden on for many miles? (yes/no)\n" << endl;
 
-	cout << "Is your animal also a main character in the film 'Shrek'? (yes/no)" << endl;
+	cout << "Is your animal also a main character in the film 'Shrek'? (yes/no)\n" << endl;
 
-	cout << "Is it a Donkey? (yes/no)" << endl;
+	cout << "Is it a Donkey? (yes/no)\n" << endl;
 
-	cout << "Is your animal used to make bacon? (yes/no)" << endl;
+	cout << "Is your animal used to make bacon? (yes/no)\n" << endl;
 
-	cout << "Is it a Pig/Hog? (yes/no)" << endl;
+	cout << "Is it a Pig/Hog? (yes/no)\n" << endl;
 
-	cout << "Is it a Cow/Bull? (yes/no)" << endl;
+	cout << "Is it a Cow/Bull? (yes/no)\n" << endl;
 
-	cout << "Is your animal generally considered a pest? (yes/no)" << endl;
+	cout << "Is your animal generally considered a pest? (yes/no)\n" << endl;
 
-	cout << "Does your animal known for jumping? (yes/no)" << endl;
+	cout << "Does your animal known for jumping? (yes/no)\n" << endl;
 
-	cout << "Is it a Rabbit/Hare? (yes/no)" << endl;
+	cout << "Is it a Rabbit/Hare? (yes/no)\n" << endl;
 
-	cout << "Is it a Mouse/Rat? (yes/no)" << endl;
+	cout << "Is it a Mouse/Rat? (yes/no)\n" << endl;
 
-	cout << "Does your animal bark? (yes/no)" << endl;
+	cout << "Does your animal bark? (yes/no)\n" << endl;
 
-	cout << "Is it a Dog? (yes/no)" << endl;
+	cout << "Is it a Dog? (yes/no)\n" << endl;
 
-	cout << "Is it a Cat? (yes/no)" << endl;
+	cout << "Is it a Cat? (yes/no)\n" << endl;
 
-	cout << "Is your animal a type of bird? (yes/no)" << endl;
+	cout << "Is your animal a type of bird? (yes/no)\n" << endl;
 
-	cout << "Is your animal used as the primary meat for the 'KFC' chain resturants? (yes/no)" << endl;
+	cout << "Is your animal used as the primary meat for the 'KFC' chain resturants? (yes/no)\n" << endl;
 
-	cout << "Is your animal bigger than a microwave? (yes/no)" << endl;
+	cout << "Is your animal bigger than a microwave? (yes/no)\n" << endl;
 
-	cout << "Is it a Chicken/Rooster? (yes/no)" << endl;
+	cout << "Is it a Chicken/Rooster? (yes/no)\n" << endl;
 
-	cout << "Is it a Bug (literally any type of insect)? (yes/no)" << endl;
+	cout << "Is it a Bug (literally any type of insect)? (yes/no)\n" << endl;
 
-	cout << "You prick, are you seriously thinking of a Human? (yes/no)" << endl;
+	cout << "You prick, are you seriously thinking of a Human? (yes/no)\n" << endl;
 
-	cout << "Does your animal have webbed feet? (yes/no)" << endl;
+	cout << "Does your animal have webbed feet? (yes/no)\n" << endl;
 
-	cout << "Is your animal used as a model for a popular rubber bath toy? (yes/no)" << endl;
+	cout << "Is your animal used as a model for a popular rubber bath toy? (yes/no)\n" << endl;
 
-	cout << "Is it a Duck? (yes/no)" << endl;
+	cout << "Is it a Duck? (yes/no)\n" << endl;
 
-	cout << "Is it a Goose? (yes/no)" << endl;
+	cout << "Is it a Goose? (yes/no)\n" << endl;
 
-	cout << "Is your animal commonly eaten during Thanksgiving? (yes/no)" << endl;
+	cout << "Is your animal commonly eaten during Thanksgiving? (yes/no)\n" << endl;
 
-	cout << "Is it a Turkey? (yes/no)" << endl;
+	cout << "Is it a Turkey? (yes/no)\n" << endl;
 
-	cout << "Is your animal almost exclusivly black? (yes/no)" << endl;
+	cout << "Is your animal almost exclusivly black? (yes/no)\n" << endl;
 
-	cout << "Is it a Crow/Raven? (yes/no)" << endl;
+	cout << "Is it a Crow/Raven? (yes/no)\n" << endl;
 
-	cout << "Does your animal make a sound that sounds similar to the English word 'who'? (yes/no)" << endl;
+	cout << "Does your animal make a sound that sounds similar to the English word 'who'? (yes/no)\n" << endl;
 
-	cout << "Is it an Owl? (yes/no)" << endl;
+	cout << "Is it an Owl? (yes/no)\n" << endl;
 
-	cout << "Is it a small wild bird (anytype)? (yes/no)" << endl;
+	cout << "Is it a small wild bird (anytype)? (yes/no)\n" << endl;
 
-	cout << "I win! YOU LOSE" << endl;
+	cout << "I win! YOU LOSE\n" << endl;
 
-	cout << "You win..." << endl;
+	cout << "You win...Or cheated.\n" << endl;
 
-	cout << "Do you want to play again? (yes/no)" << endl;
+	cout << "Do you want to play again? (yes/no)\n" << endl;
 }
