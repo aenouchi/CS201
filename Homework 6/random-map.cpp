f36ee678eb902e6d86f1f29ccba10c11b42579cf
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-06 15:16:29 -0800

    Actual function PrintDistributionN is created

--- Homework 6/random-map.cpp
+++ Homework 6/random-map.cpp
@@ -15,6 +15,7 @@
 #include <random>
 #include <utility>
 #include <map>
+#include <set>
 
 using std::cout;
 using std::endl;
@@ -24,6 +25,11 @@ using std::random_device;
 using std::mt19937;
 using std::uniform_int_distribution;
 using std::normal_distribution;
+using std::seed_seq;
+using std::round;
+using std::fixed;
+using std::string;
+
 
 int RandomBetweenU(int first, int last)
 {
@@ -44,6 +50,30 @@ int RandomBetweenN(double first, double last)
 int RandomBetween(int first, int last)
 {
 	return rand() % last + first;
+}
+
+void PrintDistributionN(int min, int max)
+{
+	random_device r;
+
+	int mean = RandomBetween(min, max);
+
+	cout << "Randomly chosen mean:" << mean << endl;
+
+	//Generate a distribution around mean
+	seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r(), };
+	mt19937 e2(seed2);
+	normal_distribution<> normal_dist(mean, 2);
+
+	map<int, int> hist;
+	for (int n = 0; n < 10000; ++n) {
+		++hist[round(normal_dist(e2))];
+	}
+	cout << "Normal distribution around " << mean << ":\n";
+	for (auto p : hist) {
+		cout << fixed << std::setprecision(1) << std::setw(2) << p.first << ' ' << string(p.second / 200, '*') << endl;
+
+	}
 }
 
 int main()
