9cfc0cad34da6850382cdd4864a7abcfb19558b1
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-07 16:51:49 -0800

    basics

---
+++ Lab 24/main.cpp
@@ -0,0 +1,19 @@
+/**
+*main.cpp
+*Homework 6
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 4,2020
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+
+int main()
+{
+
+}
