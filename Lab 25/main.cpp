72705dffdd653c0160cd2262453048ba5eaadc18
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-08 19:42:36 -0800

    Output, and determines where it is different. But crashes when vectors are the same.

--- Lab 25/main.cpp
+++ Lab 25/main.cpp
@@ -6,7 +6,7 @@
 *Jon Genetti
 *April 8,2020
 */
-
+//1, 2, 3, 5, 6, 7, 4, 8, 77, 9
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -20,7 +20,22 @@ using std::pair;
 int main()
 {
 	vector <int> set1 { 4, 5, 2, 1, 23, 63, 45, 99, 34, 9 };
-	vector <int> set2{ 1, 2, 3, 5, 6, 7, 4, 8, 9, 77 };
+	vector <int> set2{ 4, 5, 2, 1, 23, 63, 45, 99, 34, 8 };
+	pair <int, int> same;
 
+	auto nu = mismatch(set1.begin(), set1.end(), set2.begin());
+	same.first = *nu.first;
+	same.second = *nu.second;
+
+	if (same.first == set1.size())
+	{
+		cout << " Both vectors are identical" << endl;
+	}
+	
+	{
+		cout << " The vectors differ at:" << endl;
+		cout << "Set One:" << same.first << endl;
+		cout << "Set Two:" << same.second << endl;
+	}
 
 }
