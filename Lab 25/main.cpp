57592da297987595eea67c2cf378b8a7a9d13611
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-08 19:29:56 -0800

    vectors created

---
+++ Lab 25/main.cpp
@@ -0,0 +1,26 @@
+/**
+*main.cpp
+*Lab 25
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 8,2020
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::pair;
+
+int main()
+{
+	vector <int> set1 { 4, 5, 2, 1, 23, 63, 45, 99, 34, 9 };
+	vector <int> set2{ 1, 2, 3, 5, 6, 7, 4, 8, 9, 77 };
+
+
+}
