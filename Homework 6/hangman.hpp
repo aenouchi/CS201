1ac1c19c75bbcd63d14146e295c45b02301b1be5
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-09 09:47:34 -0800

    Shows letters that are used

--- Homework 6/hangman.hpp
+++ Homework 6/hangman.hpp
@@ -7,3 +7,36 @@
 *April 4,2020
 */
 
+#include <iostream>
+#include <vector>
+#include <string>
+#ifndef hangman.hpp
+#define hangman.hpp
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+using std::vector;
+
+string answer = "jazz";
+
+vector<char> cgoal;
+vector<char> got;
+vector<char> guessed;
+
+auto printGuessed = [](vector<char> f) // Shows letters that are used
+{
+	if (guessed.size() != 0)
+	{
+		cout << "Used letters" << endl;
+
+		for (char cha : f)
+		{
+			cout << cha << ", ";
+		}
+	}
+};
+
+
+#endif
