c6ccfe0f56c39157d71a4f17f9b985851f3743c3
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-09 16:41:24 -0800

    structure cart created

--- Homework 6/shopping.hpp
+++ Homework 6/shopping.hpp
@@ -20,5 +20,41 @@ using std::cin;
 using std::map;
 using std::string;
 
+struct cart
+{
+	void setPrice(double d)
+	{
+		unitPrice = d;
+	}
+
+	void setUnits(int u)
+	{
+		units = u;
+	}
+
+	int getUnits()
+	{
+		return units;
+	}
+
+	double getPrice()
+	{
+		return unitPrice;
+	}
+
+	double cost()
+	{
+		return unitPrice * units;
+	}
+
+private:
+	double unitPrice = 0;
+	int units = 0;
+};
+
+void add(cart c, int i);
+
+void remove(cart c, int i);
 
 #endif
+
