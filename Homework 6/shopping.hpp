6d1f6c9c55403d9df75800882bafc685167dd88d
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-09 16:37:47 -0800

    headers for hpp

---
+++ Homework 6/shopping.hpp
@@ -0,0 +1,24 @@
+/**
+*shopping.hpp
+*Homework 6
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 9,2020
+*/
+
+#ifndef shoping_hpp
+#define shoping_hpp
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::map;
+using std::string;
+
+
+#endif
