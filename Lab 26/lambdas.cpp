c435f40c9d935efef32f141135aca6e144066d31
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-13 13:18:10 -0800

    First compile

---
+++ Lab 26/lambdas.cpp
@@ -0,0 +1,15 @@
+/**
+*main.cpp
+*Lab 26
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 13,2020
+*/
+
+#include <iostream>
+
+int main()
+{
+
+}
---
+++ Lab 26/lambdas.hpp
@@ -0,0 +1,11 @@
+/**
+*main.cpp
+*Lab 26
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 13,2020
+*/
+
+#include <iostream>
+
