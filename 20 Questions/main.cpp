91010f0502802a9230768e1c9491a1d21f916e24
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-14 18:47:13 -0800

    Bug path added

--- 20 Questions/main.cpp
+++ 20 Questions/main.cpp
@@ -282,6 +282,20 @@ int main()
 
 			}
 
+			else if (input == "no" || input == "NO" || input == "No" || input == "n" || input == "N")
+			{
+				cout << "Is it a Bug (literally any type of Insect/Arachnid)? (yes/no)\n" << endl;
+
+				cin >> input;
+
+				if (input == "yes" || input == "YES" || input == "Yes" || input == "y" || input == "Y")
+				{
+					cout << "I win! YOU LOSE\n" << endl;
+
+					exit;
+				}
+			}
+
 		
 		}
 	}
@@ -300,11 +314,7 @@ int main()
 
 			
 
-				cout << "Is your animal used as the primary meat for the 'KFC' chain resturants? (yes/no)\n" << endl;
-
-				cout << "Is your animal bigger than a microwave? (yes/no)\n" << endl;
-
-				cout << "Is it a Chicken/Rooster? (yes/no)\n" << endl;
+			
 
 				cout << "Is it a Bug (literally any type of insect)? (yes/no)\n" << endl;
 
