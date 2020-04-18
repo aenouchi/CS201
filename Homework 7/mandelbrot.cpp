eda7d2de66d70537e096b861b243d08a31726521
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-18 12:30:54 -0800

    first compile

---
+++ Homework 7/mandelbrot.cpp
@@ -0,0 +1,20 @@
+/**
+*mandelbrot.cpp
+*Homework 7
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 13,2020
+*/
+
+#include <iostream>
+#include "Color3.hpp"
+#include "Image3.hpp"
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+
+}
