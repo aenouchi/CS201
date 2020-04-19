51732dec0ccb1f85ae8ee8c72bc5ee090da7073c
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-18 18:04:58 -0800

    Try again

---
+++ Homework 7/mandelbrot.cpp
@@ -0,0 +1,21 @@
+/**
+*mandelbrot.cpp
+*Homework 7
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 13,2020
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+using std::ifstream;
+using std::ofstream;
+
+int main()
+{
+
+}
