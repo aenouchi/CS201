0815cce0ac246ff3658a049fe05639a123badde8
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-22 20:51:38 -0800

    public member functions added

--- Lab 33/Source.cpp
+++ Lab 33/Source.cpp
@@ -14,6 +14,42 @@ using std::endl;
 using std::cin;
 using std::to_string;
 
+class sum {
+	int a;
+	double b;
+public:
+	void setter(char c, double d)
+	{
+		if (c == 'a' || c == 'A')
+		{
+			a = b;
+		}
+		else
+		{
+			b = b;
+		}
+	}
+
+	double getter(char c)
+	{
+		if (c == 'a' || c == 'A')
+		{
+			return a;
+		}
+		else
+		{
+			return b;
+		}
+	}
+
+	void print()
+	{
+		auto p = to_string(a);
+		auto q = to_string(b);
+
+		cout << p << ", " << q << endl;
+	}
+};
 
 int main()
 {
