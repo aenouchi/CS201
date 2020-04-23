ec2f625424dc1a2d23fd30d48951b13d2a503ba9
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-22 20:47:25 -0800

    First compile

---
+++ Lab 33/Source.cpp
@@ -0,0 +1,19 @@
+/**
+*Source.cpp
+*Lab 33
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 22,2020
+*/
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+int main()
+{
+
+}
