140885074267674a813d40ff15d4da273c5e6cec
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-23 18:47:59 -0800

    Removed dupilcate

---
+++ Homework 8/Environment.hpp
@@ -0,0 +1,36 @@
+/**
+*environment.hpp
+*Homework 8
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 22,2020
+*/
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+#ifndef Environment_hpp
+#define Environment_hpp	
+
+
+class Environment {// Prototype for environment function
+		int temp;
+		bool heater;
+
+	public:
+		Environment();
+		Environment(int t, bool h);
+
+
+	
+		void iteration();
+
+		int getTemp();
+		bool getHeater();
+		void setHeater(bool b);
+	};
+
+#endif 	
