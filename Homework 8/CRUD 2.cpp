44afd66d1946d25b1d29f82e03c3f26c8853b0a7
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-24 09:24:52 -0800

    CRUD files created

---
+++ Homework 8/CRUD 2.cpp
@@ -0,0 +1,8 @@
+/**
+*CRUD 2.cpp
+*Homework 8
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 24,2020
+*/
---
+++ Homework 8/CRUD 2.hpp
@@ -0,0 +1,8 @@
+/**
+*CRUD 2.hpp
+*Homework 8
+*Aaron Nouchi
+*CS 201
+*Jon Genetti
+*April 24,2020
+*/
