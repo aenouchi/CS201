b48d6d86964dcfef14d8fa22370ae3a102112b23
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-24 10:04:30 -0800

    'CRUD' defined

--- Homework 8/CRUD 2.cpp
+++ Homework 8/CRUD 2.cpp
@@ -18,5 +18,42 @@ using std::string;
 using std::map;
 using std::pair;
 
+void CRUD::create(map<string, string>& q, string w, string e)//Define create
+{
+	q.insert(pair<string, string>(w, e));
+}
 
-
+string CRUD::read(pair<string, string> p)//Define read
+{
+	return p.second;
+}
+string CRUD::read(pair<string, string> p, int x)
+{
+	if (x == 2)
+		return p.second;
+	else
+		return p.first;
+}
+string CRUD::read(map<string, string> q, string w)
+{
+	return q[w];
+}
+string CRUD::read(map<string, string> q, string w)
+{
+	return q[w];
+}
+void CRUD::read(map<string, string> q)
+{
+	for (pair<string, string> p : q)
+	{
+		cout << read(p, 1) << ": " << read(p, 2) << endl;
+	}
+}
+void CRUD::update(map<string, string>& q, string w, string e)//Define update
+{
+	q[w] = e;
+}
+void CRUD::Delete(map<string, string>& q, string w)//Define delete
+{
+	q.erase(w);
+}
