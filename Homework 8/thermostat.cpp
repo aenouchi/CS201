1376ba3a98dff89fac16f788d5e9b2b45c040268
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-23 20:48:09 -0800

    Loops for user input

--- Homework 8/thermostat.cpp
+++ Homework 8/thermostat.cpp
@@ -8,12 +8,31 @@
 */
 #include <iostream>
 #include <string>
+#include "environment.hpp"
+#include "simulator.hpp"
 
 using std::cout;
 using std::endl;
 using std::cin;
 using std::string;
 
-int main()
+int main() //Loops for user feedback and temp updates
 {
-}
+	int defaultroomtemp = 75;
+	int defaultthermo = 65;
+		
+
+		cout << "The start of thermostat..." << endl << endl;
+
+		Simulator sim(defaultthermo, defaultroomtemp);
+		cout << "Default thermostat is set to " << defaultroomtemp << " degrees." << endl;
+		cout << "Default room tempurature is set to " << defaultthermo << " degrees." << endl;
+		cout << endl;
+
+		bool go = true;
+		while (go)
+		{
+			sim.ask();
+			sim.run();
+		}
+	}
