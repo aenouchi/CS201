d248fb68757acdf5f079c7175cb93e4cf3d9bc8d
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-24 18:55:21 -0800

    home.hpp defined

--- Homework 8/home.hpp
+++ Homework 8/home.hpp
@@ -7,6 +7,11 @@
 *April 22,2020
 */
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <random>
+#include <stdlib.h>
+
 #include "Agent.Hpp" 
 #ifndef home_HPP
 #define home_HPP 
@@ -14,5 +19,16 @@
 using std::cout;
 using std::endl;
 using std::cin;
+using std::vector;
+using std::mt19937;
+
+class Environment;
+class Agent;
+
+void clearConsole();
+void waitForContinue();
+void printState(Environment& env, Agent& agt);
+bool rollBool(mt19937* e1);
 
 #endif
+
