01e40d8645db835556c0019c4cd7bbcf2b9ce374
Aaron Nouchi <aenouchi@alaska.edu>
2020-04-24 18:58:43 -0800

    headers defined in home.cpp

--- Homework 8/home.cpp
+++ Homework 8/home.cpp
@@ -7,9 +7,16 @@
 *April 22,2020
 */
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <random>
+#include <stdlib.h>
 #include "Agent.Hpp" 
-
+#include "Environment.hpp"
+#include "Home.hpp"
 
 using std::cout;
 using std::endl;
 using std::cin;
+using std::vector;
+using std::mt19937;
